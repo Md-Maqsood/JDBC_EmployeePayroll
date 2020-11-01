fa083f4099af357efeefdbfb2176e2452a8b37f6
maqsood <maqsoodhamd37@gmail.com>
2020-11-01 20:01:17 +0530

    [Maqsood].add initial commit

---
+++ src/main/java/com/bridgelabs/employeepayrollservicejdbc/EmployeePayrollService.java
@@ -0,0 +1,12 @@
+package com.bridgelabs.employeepayrollservicejdbc;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class EmployeePayrollService {
+	private static Logger logger = LogManager.getLogger(EmployeePayrollService.class);
+
+	public static void main(String[] args) {
+		logger.info("Welcome to employee payroll sevice program.");
+	}
+}
---
+++ src/test/java/com/bridgelabs/employeepayrollservicejdbc/EmployeePayrollServiceTest.java
@@ -0,0 +1,20 @@
+package com.bridgelabs.employeepayrollservicejdbc;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class EmployeePayrollServiceTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
